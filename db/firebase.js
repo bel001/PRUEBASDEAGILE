77b47b0c8d9fa82d98d03e8be0d35392a7f3b90e
bel001 <rodrigoabel2801@gmail.com>
2025-12-09 14:30:17 -0500

    Simplify Firebase config - use only serviceAccountKey.json

--- db/firebase.js
+++ db/firebase.js
@@ -1,21 +1,6 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-// Configuración usando variables de entorno (Render) o archivo local (desarrollo)
-let serviceAccount;
-
-if (process.env.FIREBASE_PRIVATE_KEY) {
-  // Producción: usar variables de entorno
-  serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
-  };
-  console.log('🔥 Firebase: Usando variables de entorno');
-} else {
-  // Desarrollo local: usar archivo JSON
-  serviceAccount = require('../serviceAccountKey.json');
-  console.log('🔥 Firebase: Usando serviceAccountKey.json');
-}
+const serviceAccount = require("../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
